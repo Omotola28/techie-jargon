a60c90b6328673048582e69edddfdc8dcb913372
Omotola28 <o.m.shogunle@edu.salford.ac.uk>
2021-09-16 18:31:37 +0100

    changed back

--- public/customjs.js
+++ public/customjs.js
@@ -1,7 +1,7 @@
 //Mutate card items when sidebar is toggled
 window.addEventListener("load", (event) => {
 
-  const screenWidthA = window.matchMedia("(max-width:1430px)");
+  const screenWidthA = window.matchMedia("(max-width:1440px)");
   const screenWidthB = window.matchMedia("(max-width: 800px)");
   const sideBar = document.querySelector(".nav-menu");
   const noOfHits = document.querySelector(".noOfHits");
