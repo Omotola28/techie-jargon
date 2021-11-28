ddf9ef76ce62d215ae0f1ab2341220a39fa7c71d
Omotola28 <o.m.shogunle@edu.salford.ac.uk>
2021-11-28 12:09:30 +0000

    making merge conflicts

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import HomePage from './components/HomePage';
 function App() {
   return (
     <>
+        //making conflicting changes here
       <div className="container">
         <HomePage/>
       </div>
     </>
-   
+
   );
 }
 
