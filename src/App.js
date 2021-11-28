e9f5846d44b75423c4817585de57018ed472824c
Omotola Matilda Shogunle <o.m.shogunle@edu.salford.ac.uk>
2021-11-28 12:05:34 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './components/HomePage';
 function App() {
   return (
     <>
+      //Practicing merge conflicts 
       <div className="container">
         <HomePage/>
       </div>
